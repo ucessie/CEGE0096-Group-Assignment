349a053e82878f97d69def7a9ad66b7229d383cc
ucessie <ucessie@ucl.ac.uk>
2019-12-18 13:08:16 +0000

    add bounding type

--- user_input.py
+++ user_input.py
@@ -5,13 +5,24 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-#file input
-print('Load File')
-loadfile = ('/Users/joseph/Desktop/UCL/Geospatial programming/Material/background/raster-50k_2724246.tif')
+class input():
+    def __init__(self,):
 
-#load in rasterio
-geodata = rasterio.open(loadfile)
 
-#Bounding box
-print("Raster bounding box: ", geodata.bounds)
-print("Raster type: ", type(geodata.bounds))
+
+
+
+    def input():
+
+        #file input
+        print('Load File')
+        loadfile = ('/Users/joseph/Desktop/UCL/Geospatial programming/Material/background/raster-50k_2724246.tif')
+
+        #load in rasterio
+        geodata = rasterio.open(loadfile)
+
+        #Bounding box
+        print("Raster bounding box: ", geodata.bounds)
+        print("Raster left bound: ", geodata.bound.left)
+        print("Raster right bound: ", geodata.bound.right)
+        print("Raster type: ", type(geodata.bounds))
