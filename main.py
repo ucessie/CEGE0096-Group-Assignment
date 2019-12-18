f9733ea28032768b6933d66a1d8ad1f5e4293b2e
ucessie <ucessie@ucl.ac.uk>
2019-12-18 13:27:55 +0000

    Simple test

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import user_input as io
+from user_input import IO
+
+
+
+
+
 
 print("Enter the user location (as Easting and Northing)")
-user_point = input("location: ")
+
+#Input user point
+x, y = IO.user_input()
+raster = IO.raster_input()
+
+#Bounding box
+print("Raster bounding box: ", raster.bounds)
+print("Raster left bound: ", raster.bounds.left)
+print("Raster right bound: ", raster.bounds.right)
+print("Raster type: ", type(raster.bounds.left))
--- user_input.py
+++ user_input.py
@@ -5,24 +5,22 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-class input():
-    def __init__(self,):
+class IO:
 
-
-
-
-
-    def input():
+    def raster_input():
 
         #file input
-        print('Load File')
         loadfile = ('/Users/joseph/Desktop/UCL/Geospatial programming/Material/background/raster-50k_2724246.tif')
 
         #load in rasterio
         geodata = rasterio.open(loadfile)
 
-        #Bounding box
-        print("Raster bounding box: ", geodata.bounds)
-        print("Raster left bound: ", geodata.bound.left)
-        print("Raster right bound: ", geodata.bound.right)
-        print("Raster type: ", type(geodata.bounds))
+
+        return geodata
+
+
+    def user_input():
+
+                x = float(input("x coordinate: "))
+                y = float(input("y coordinate: "))
+                return x,y
